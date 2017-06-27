876dd64a49eb350a49f7cdd6b60b989846afd8e5
zxf <562242780@qq.com>
2017-06-27 22:45:13 +0800

    完善闪照的延时

--- app/src/main/java/cn/android/jkbd/SplashScreen.java
+++ app/src/main/java/cn/android/jkbd/SplashScreen.java
@@ -1,6 +1,8 @@
 package cn.android.jkbd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,5 +15,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        countDownTimer.start();
     }
+    CountDownTimer countDownTimer = new CountDownTimer(4000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            startActivity(new Intent(SplashScreen.this,MainActivity.class));
+        }
+    };
 }
